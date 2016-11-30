7bcc1f5cc1637b5ac8b42da29b8e72fca6c9048d
Emily Mabrey <emilymabrey93@gmail.com>
2016-11-30 15:33:04 -0500

    Fix and cleanup Travis-CI build config
    Removed the Java 7 execution from .travis.yml because it was not
    compatible with the enforcement rules of the project POMs and modified
    the variables used to control the custom JDK install:
    
    OVERRIDE_JDK => INSTALL_JDK
    JDK => [removed]
    
    Cleaned and refactored the Travis-CI build scripts, including
    addAptRepos and installAptPrograms, and added various sanity checks and
    input verifications. Now if a validation error is encountered an error
    message is printed to the output log of the CI build. Improved support
    for custom JDK installations, including support for Java 6, Java 7,
    Java 8, and the as yet unreleased Java 9 JDK.
    
    Made an additional minor change to the emabrey-parent POM- the
    "versions.java.minimum" property was renamed to
    "versions.buildJDK.minimum". This modification will make the distinction
    between the build JDK and the target JDK more obvious should future
    children wish to target a different version via backwards compatibility.
    
    Signed-off-by: Emily Mabrey <emilymabrey93@gmail.com>

--- .travis/scripts/addAptRepos.sh
+++ .travis/scripts/addAptRepos.sh
@@ -2,25 +2,27 @@
 
 set -evu -o pipefail
 
-#If a custom JDK is used this installs the correct repository for the specific JDK
-
-if [[ "${OVERRIDE_JDK}" = 'true' ]]
+if ! [[ "${INSTALL_JDK}" = 'true' ]]
 then
-    sudo apt-get purge openjdk* oracle-java* -q -y
-    sudo apt-get autoclean -q -y
-
-    if [[ "${JDK_TYPE}" = 'open' ]]
-    then
-        sudo add-apt-repository ppa:openjdk-r/ppa -y
-    elif [[ "${JDK_TYPE}" = 'zulu' ]]
-    then
-        #Private repository via http://repos.azulsystems.com/
-        sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0x219BD9C9
-        sudo apt-add-repository "deb http://repos.azulsystems.com/ubuntu stable main"
-    elif [[ "${JDK_TYPE}" = 'oracle' ]]
-    then
-        sudo add-apt-repository ppa:webupd8team/java -y
-    fi
-
-    sudo apt-get update -q -y
+    #Skipping custom JDK install
+    echo "Skipped: \$\{INSTALL_JDK\} is not true, so APT repositories for the custom JDKs are not needed."
+    exit 0;
 fi
+
+sudo apt-get purge openjdk* oracle-java* -qq -y
+sudo apt-get autoclean -qq -y
+
+if [[ "${JDK_TYPE}" = 'open' ]]
+then
+    sudo add-apt-repository ppa:openjdk-r/ppa -y
+elif [[ "${JDK_TYPE}" = 'zulu' ]]
+then
+    #Private repository via http://repos.azulsystems.com/
+    sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0x219BD9C9
+    sudo apt-add-repository "deb http://repos.azulsystems.com/ubuntu stable main"
+elif [[ "${JDK_TYPE}" = 'oracle' ]]
+then
+    sudo add-apt-repository ppa:webupd8team/java -y
+fi
+
+sudo apt-get update -qq -y
--- .travis/scripts/installAptPrograms.sh
+++ .travis/scripts/installAptPrograms.sh
@@ -2,58 +2,65 @@
 
 set -evu -o pipefail
 
-if [[ "${OVERRIDE_JDK}" = 'true' ]] #If Installing Custom JDK
+#Setup needed variables based upon CPU ARCH
+if uname -a | grep x86_64 >/dev/null;
 then
-    if uname -a | grep x86_64 >/dev/null;
-    then
-        #64bit arch
-        ARCH_SUFFIX=amd64
-        JVM_LIBS_DIR=/usr/lib/jvm
-    else
-        #32bit arch
-        ARCH_SUFFIX=i386
-        JVM_LIBS_DIR=/usr/lib/jvm
-    fi
-
-    if [[ "${JDK_TYPE}" = 'open' ]] && [[ "${JDK}" = 'openjdk8' ]]
-    then
-        #JDK is openjdk8 custom with JavaFx 8
-        sudo apt-get install openjdk-8 openjfx -y
-        JVM_ID=java-1.8.0-openjdk-${ARCH_SUFFIX}
-    elif [[ "${JDK_TYPE}" = 'oracle' ]] && [[ "${JDK}" = 'oraclejdk8' ]]
-    then
-        #JDK is oraclejdk8 custom
-        echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
-        echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
-        sudo apt-get install --no-install-recommends oracle-java8-installer -y
-        JVM_ID=java-8-oracle
-    elif [[ "${JDK_TYPE}" = 'zulu' ]]
-    then
-        if [[ "${JDK}" = 'openjdk8' ]]
-        then
-            #JDK is openjdk8 custom from Zulu
-            sudo apt-get install zulu-8 -y
-            JVM_ID=zulu-8-${ARCH_SUFFIX}
-        elif [[ "${JDK}" = 'openjdk7' ]]
-        then
-            #JDK is openjdk7 custom from Zulu
-            sudo apt-get install zulu-7 -y
-            JVM_ID=zulu-7-${ARCH_SUFFIX}
-        fi
-    else
-
-        echo "Fail: \$OVERRIDE_JDK is true but a \$JDK_TYPE value of ${JDK_TYPE} is unsupported"
-        exit 1;
-    fi
-    
-    #Update the CI environment JAVA_HOME
-    # Clunky workaround so that default Travis-CI JAVA_HOME (/usr/lib/jvm/java-7-oracle/) actually points to our custom
-    # installed JDK. The correct way to do this would be to change the bash profile initialization, but that would be
-    # even uglier to accomplish within a non-interactive environment. Without creating this symoblic link our changes to
-    # JAVA_HOME would be undone as soon as the next shell is created for the next Travis-CI lifecycle phase
-    export JAVA_HOME=${JVM_LIBS_DIR}/${JVM_ID}
-    sudo ln -s ${JAVA_HOME} /usr/lib/jvm/java-7-oracle
-
-    #Show current Java version for debug purposes
-    sudo java -version
+    #64bit arch
+    MIN_JDK_VER=6
+    MAX_JDK_VER=9
+    VALID_JDK_VER="^[${MIN_JDK_VER}-${MAX_JDK_VER}]+$"
+    ARCH_SUFFIX=amd64
+    JVM_LIBS_DIR=/usr/lib/jvm
+else
+    #32bit arch
+    MIN_JDK_VER=6
+    MAX_JDK_VER=9
+    VALID_JDK_VER="^[${MIN_JDK_VER}-${MAX_JDK_VER}]+$"
+    ARCH_SUFFIX=i386
+    JVM_LIBS_DIR=/usr/lib/jvm
 fi
+
+if ! [[ "${INSTALL_JDK}" = 'true' ]]
+then
+    echo "Skipped: \$\{INSTALL_JDK\} is not true, so custom JDK install is being skipped."
+    exit 0;
+elif ! [[ ${JDK_VER} =~ ${VALID_JDK_VER} ]]
+then
+    echo "Fail: \$\{JDK_VER\} value of ${JDK_VER} is invalid. Required version: [${MIN_JDK_VER},${MAX_JDK_VERSION}]"
+    exit 1;
+fi
+
+if [[ "${JDK_TYPE}" = 'open' ]] 
+then
+    JDK_PACKAGE=openjdk-${JDK_VER}
+    JVM_ID=java-1.${JDK_VER}.0-openjdk-${ARCH_SUFFIX}
+    sudo apt-get install "${JDK_PACKAGE}" -y
+elif [[ "${JDK_TYPE}" = 'oracle' ]]
+then
+    JDK_PACKAGE=oracle-java${JDK_VER}-installer
+    JVM_ID=java-${JDK_VER}-oracle
+    echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
+    echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
+    sudo apt-get install --no-install-recommends "${JDK_PACKAGE}" -y
+elif [[ "${JDK_TYPE}" = 'zulu' ]]
+then
+    JDK_PACKAGE=zulu-${JDK_VER}
+    JVM_ID=zulu-${JDK_VER}-${ARCH_SUFFIX}
+    sudo apt-get install "${JDK_PACKAGE}" -y
+else
+    echo "Fail: \$\{JDK_TYPE\} value of ${JDK_TYPE} is unsupported. Supported types: open, oracle, zulu"
+    exit 1;
+fi
+
+#Update the CI environment JAVA_HOME
+# Clunky workaround so that default Travis-CI JAVA_HOME (/usr/lib/jvm/java-7-oracle/) actually points to our custom
+# installed JDK. The correct way to do this would be to change the bash profile initialization, but that would be
+# even uglier to accomplish within a non-interactive environment. Without creating this symbolic link our changes to
+# JAVA_HOME would be undone as soon as the next shell is created for the next Travis-CI lifecycle phase
+export JAVA_HOME=${JVM_LIBS_DIR}/${JVM_ID}
+sudo ln -s "${JAVA_HOME}" /usr/lib/jvm/java-7-oracle
+
+#Show current Java versions for debug purposes
+echo "PATH Java version is $(java -version 2>&1 | awk '/java version/ {print $3}' | egrep -o '[^\"]*')"
+echo "PATH Javac version is $(javac -version 2>&1 | awk '/javac/ {print $2}' | egrep -o '[^\"]*')"
+echo "Java version of Maven is $(mvn --version 2>&1 | awk '/Java version/ {print $3}' | egrep -o '[^\"]*')"
