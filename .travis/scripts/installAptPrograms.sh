4c3b51506aa1cdafa645e4f8d0bf5390cb466490
Emily Mabrey <emilymabrey93@gmail.com>
2016-11-30 05:23:48 -0500

    Reattempt fixing Travis-CI build errors
    Add the missing path separator that was making the
    "update-jave-alternatives" script incorrectly process the path to the
    custom JDK install. Also, an additional debug statement is being added
    to help figure out the problem should the build fail or error again.
    
    Signed-off-by: Emily Mabrey <emilymabrey93@gmail.com>

--- .travis/scripts/installAptPrograms.sh
+++ .travis/scripts/installAptPrograms.sh
@@ -19,30 +19,33 @@ then
     then
         #JDK is openjdk8 custom with JavaFx 8
         sudo apt-get install openjdk-8 openjfx -y
-        JVM_DIR=${JVM_LIBS_DIR}/java-1.8.0-openjdk-${ARCH_SUFFIX}
+        JVM_DIR=${JVM_LIBS_DIR}/java-1.8.0-openjdk-${ARCH_SUFFIX}/
     elif [[ "${JDK_TYPE}" = 'oracle' ]] && [[ "${JDK}" = 'oraclejdk8' ]]
     then
         #JDK is oraclejdk8 custom
         sudo apt-get install --no-install-recommends oracle-java8-installer -y
-        JVM_DIR=${JVM_LIBS_DIR}/java-8-oracle
+        JVM_DIR=${JVM_LIBS_DIR}/java-8-oracle/
     elif [[ "${JDK_TYPE}" = 'zulu' ]]
     then
         if [[ "${JDK}" = 'openjdk8' ]]
         then
             #JDK is openjdk8 custom from Zulu
             sudo apt-get install zulu-8 -y
-            JVM_DIR=${JVM_LIBS_DIR}/zulu-8-${ARCH_SUFFIX}
+            JVM_DIR=${JVM_LIBS_DIR}/zulu-8-${ARCH_SUFFIX}/
         elif [[ "${JDK}" = 'openjdk7' ]]
         then
             #JDK is openjdk7 custom from Zulu
             sudo apt-get install zulu-7 -y
-            JVM_DIR=${JVM_LIBS_DIR}/zulu-7-${ARCH_SUFFIX}
+            JVM_DIR=${JVM_LIBS_DIR}/zulu-7-${ARCH_SUFFIX}/
         fi
     else
 
         echo "Fail: \$OVERRIDE_JDK is true but a \$JDK_TYPE value of ${JDK_TYPE} is unsupported"
         exit 1;
     fi
+
+    #List all installed Java alternatives for debug purposes
+    sudo update-java-alternatives -l
 
     #Update the CI environment to use the custom JDK we just installed
     sudo update-java-alternatives -s ${JVM_DIR}
